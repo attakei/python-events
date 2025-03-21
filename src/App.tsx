7997261dc53439c04af1ea8785f7d3d77b741747
Kazuya Takei <myself@attakei.net>
2025-03-21 15:33:28 +0900

    refactor: Split component

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,13 @@
-import L from 'leaflet';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './App.css';
-import { Icon } from 'leaflet';
-import markerIconPng from 'leaflet/dist/images/marker-icon.png';
 import { events, geocodes } from './assets';
+import MapView from './components/MapView';
 
 function App() {
-  const worldBounds = L.latLngBounds([-90, -180], [90, 180]);
-  const maxBounds = L.latLngBounds(L.latLng(-90, -180), L.latLng(90, 180));
-
   return (
     <>
       <h1>Python Events</h1>
-      <MapContainer
-        bounds={worldBounds}
-        maxBounds={maxBounds}
-        center={[30, 0]}
-        zoom={2}
-        style={{
-          height: '50vh',
-          maxWidth: '50vw',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-        }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {events
-          .filter((e) => e.location in geocodes)
-          .map((e) => (
-            <Marker
-              position={geocodes[e.location]}
-              key={e.uid}
-              icon={
-                new Icon({
-                  iconUrl: markerIconPng,
-                  iconSize: [25, 41],
-                  iconAnchor: [12, 41],
-                })
-              }
-            >
-              <Popup>{e.title}</Popup>
-            </Marker>
-          ))}
-      </MapContainer>
+      <MapView events={events} geocodes={geocodes} />
     </>
   );
 }
---
+++ src/components/MapView.tsx
@@ -0,0 +1,57 @@
+/**
+ * Map display component
+ */
+import L from 'leaflet';
+import { Icon } from 'leaflet';
+import markerIconPng from 'leaflet/dist/images/marker-icon.png';
+import type { FC } from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import type { CalendarEvent, GeocodeData } from '../types';
+
+const worldBounds = L.latLngBounds([-90, -180], [90, 180]);
+const maxBounds = L.latLngBounds(L.latLng(-90, -180), L.latLng(90, 180));
+
+export type Props = {
+  events: CalendarEvent[];
+  geocodes: GeocodeData;
+};
+
+export const Component: FC<Props> = ({ events, geocodes }) => {
+  const visibleEvents = events.filter((e) => e.location in geocodes);
+  return (
+    <MapContainer
+      bounds={worldBounds}
+      maxBounds={maxBounds}
+      center={[30, 0]}
+      zoom={2}
+      style={{
+        height: '50vh',
+        maxWidth: '50vw',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+      }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {visibleEvents.map((e) => (
+        <Marker
+          position={geocodes[e.location]}
+          key={e.uid}
+          icon={
+            new Icon({
+              iconUrl: markerIconPng,
+              iconSize: [25, 41],
+              iconAnchor: [12, 41],
+            })
+          }
+        >
+          <Popup>{e.title}</Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+};
+
+export default Component;
