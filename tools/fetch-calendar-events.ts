aa0140c4f297b18fb8584c31afb27a118b77afdc
Kazuya Takei <myself@attakei.net>
2025-03-21 10:53:07 +0900

    feat: Aggregate events when fetching

--- tools/fetch-calendar-events.ts
+++ tools/fetch-calendar-events.ts
@@ -3,6 +3,7 @@
  */
 
 import { readFileSync, writeFileSync } from 'node:fs';
+import { writeFile } from 'node:fs/promises';
 import { resolve } from 'node:path';
 import { program } from 'commander';
 import { parse } from 'smol-toml';
@@ -13,41 +14,50 @@ type CalendarMasterData = { [key: string]: CalendarMaster };
 
 const toISODateString = (d: Date): string => d.toISOString().slice(0, 10);
 
-program.option('--out <out>').action(async (options) => {
-  const now = new Date();
-  // Load calendar master.
-  const calendars: CalendarMasterData = (() => {
-    const settingsToml = resolve(import.meta.dirname, '..', 'settings.toml');
-    const data = parse(readFileSync(settingsToml).toString());
-    return data.calendars as CalendarMasterData;
-  })();
+program
+  .option('--out <out>')
+  .option('-a, --aggregate')
+  .action(async (options) => {
+    const now = new Date();
+    // Load calendar master.
+    const calendars: CalendarMasterData = (() => {
+      const settingsToml = resolve(import.meta.dirname, '..', 'settings.toml');
+      const data = parse(readFileSync(settingsToml).toString());
+      return data.calendars as CalendarMasterData;
+    })();
 
-  const promises = Object.entries(calendars).map(async ([group, cal]) => {
-    const icsText = await fetch(cal.url).then((resp) => resp.text());
-    const calendar = convertIcsCalendar(undefined, icsText);
-    const events: CalendarEvent[] = (calendar.events || [])
-      .map((e) => {
-        return {
-          group,
-          uid: e.uid,
-          title: e.summary,
-          begin: toISODateString(e.start.date),
-          end: toISODateString((e.end || e.start).date),
-          location: e.location || '',
-          body: e.description || '',
-        };
-      })
-      // NOTE: Use only future events for initial development
-      .filter((e) => e.begin > toISODateString(now));
-    if (options.out !== undefined) {
-      const out = resolve(options.out, `events.${group}.json`);
-      console.log(options.out);
-      writeFileSync(out, JSON.stringify(events));
-    } else {
-      console.log(events);
-    }
+    const promises = Object.entries(calendars).map(async ([group, cal]) => {
+      const icsText = await fetch(cal.url).then((resp) => resp.text());
+      const calendar = convertIcsCalendar(undefined, icsText);
+      const events: CalendarEvent[] = (calendar.events || [])
+        .map((e) => {
+          return {
+            group,
+            uid: e.uid,
+            title: e.summary,
+            begin: toISODateString(e.start.date),
+            end: toISODateString((e.end || e.start).date),
+            location: e.location || '',
+            body: e.description || '',
+          };
+        })
+        // NOTE: Use only future events for initial development
+        .filter((e) => e.begin > toISODateString(now));
+      if (options.out !== undefined) {
+        const out = resolve(options.out, `events.${group}.json`);
+        console.log(options.out);
+        writeFileSync(out, JSON.stringify(events));
+      } else {
+        console.log(events);
+      }
+      return events;
+    });
+    await Promise.all(promises).then(async (eventsList) => {
+      if (options.aggregate !== undefined) {
+        const out = resolve(options.out, 'events.json');
+        await writeFile(out, JSON.stringify(eventsList.flat()));
+      }
+    });
   });
-  await Promise.all(promises);
-});
 
 program.parse();
