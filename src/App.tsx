dce2573d6d78af1101f4d057e8974f78f99429c0
Kazuya Takei <myself@attakei.net>
2025-03-21 20:20:26 +0900

    feat: Simple footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import 'bulma/css/bulma.min.css';
 import 'leaflet/dist/leaflet.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { events, geocodes } from './assets';
+import Footer from './components/Footer';
 import MapView from './components/MapView';
 import Nav from './components/Nav';
 import { Container } from 'react-bulma-components';
@@ -10,9 +11,10 @@ function App() {
   return (
     <>
       <Nav />
-      <Container style={{marginTop: 'var(--bulma-navbar-height)'}}>
+      <Container style={{marginTop: 'var(--bulma-navbar-height)', minHeight: 'calc(100vh - 170px)'}}>
         <MapView events={events} geocodes={geocodes} />
       </Container>
+      <Footer />
     </>
   );
 }
