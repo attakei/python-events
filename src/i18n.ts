efc5cde4212c6a048fe456f8b520285f162dcb53
Kazuya Takei <myself@attakei.net>
2025-03-22 03:00:16 +0900

    feat: Publish locale files outside of typescript sources

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,24 +1,19 @@
 import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend, { type HttpBackendOptions } from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: {
-        'scheduled-events': '{{count}} events are scheduled.',
-        'display-events': 'This map displays {{count}} events.',
-      },
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init<HttpBackendOptions>({
+    lng: 'en',
+    fallbackLng: ['en', 'ja'],
+    backend: {
+      loadPath: `${import.meta.env.BASE_URL}locales/{{lng}}/{{ns}}.json`,
+      addPath: `${import.meta.env.BASE_URL}/locales/add/{{lng}}/{{ns}}`,
     },
-    ja: {
-      translation: {
-        'scheduled-events': '{{count}} 個のイベントが予定されています。',
-        'display-events':
-          '地図上には {{count}} 個のイベントが表示されています。',
-      },
-    },
-  },
-  lng: 'en',
-  fallbackLng: ['en', 'ja'],
-});
+  });
 
 export default i18n;
