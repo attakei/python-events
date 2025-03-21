75b6a31445cf3ff84e05fd1a74b1e9201f0bb496
Kazuya Takei <myself@attakei.net>
2025-03-21 11:01:51 +0900

    feat: Render maps on website

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
+import L from 'leaflet';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 import { useState } from 'react';
 import './App.css';
+import events from './assets/events.json';
+import geocodes from './assets/geocodes.json';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const worldBounds = L.latLngBounds([-90, -180], [90, 180]);
+  const maxBounds = L.latLngBounds(L.latLng(-90, -180), L.latLng(90, 180));
 
   return (
     <>
       <h1>Python Events</h1>
+      <MapContainer
+        bounds={worldBounds}
+        maxBounds={maxBounds}
+        center={[30, 0]}
+        zoom={2}
+        style={{
+          height: '50vh',
+          maxWidth: '50vw',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+        }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {events
+          .filter((e) => e.location in geocodes)
+          .map((e) => (
+            <Marker position={geocodes[e.location]} key={e.uid}>
+              <Popup>{e.title}</Popup>
+            </Marker>
+          ))}
+      </MapContainer>
     </>
   );
 }
